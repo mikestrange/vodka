d76f6c4ca45bc526abf32a810bcbf5f00eb38d5d
MikeRiy <542540443@qq.com>
2018-03-13 21:45:48 +0800

    sh.1.1.2

--- make.sh
+++ make.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+# ps aux | grep godark
+
 NAME="godark"
 #这里关闭当前进程
-ID=`ps -ef | grep "$NAME" | grep -v "$0" | grep -v "grep" | awk '{print $2}'`
+#ID=`ps -ef | grep "$NAME" | grep -v "$0" | grep -v "grep" | awk '{print $2}'`
+ID=`ps aux | grep "$NAME" | grep -v "grep" | awk '{print $2}'`
 echo "---------------"
 for id in $ID
 do
@@ -10,6 +13,12 @@ kill -9 $id
 echo "killed $id"
 done
 echo "---------------"
+
+if [ $# == 0 ]; then
+    echo "no args"
+    exit
+fi
+
 ##
 dir=$(cd `dirname $0`; pwd)
 echo $dir
@@ -18,7 +27,8 @@ export GOPATH=$dir
 #编辑进程名称
 go build -o $NAME $dir/src/main.go
 cd $dir
-./$NAME $0 > debug.log &
+./$NAME $@ > debug.log &
+echo "start server:"$@
 #上面已经进入后台
 #bg %1
 
--- upload.sh
+++ upload.sh
@@ -7,7 +7,10 @@ echo $list
 
 for data in ${list[@]}
 do
-scp -r ${data} root@120.77.149.74:~/home/godark
+scp -r ${data} root@hotbeel.com:~/home/godark
 done
+
+#直接运行服务器
+ssh root@hotbeel.com "cd ~/home/godark; ls -a;sh make.sh ser;exit;"
 
 exit
